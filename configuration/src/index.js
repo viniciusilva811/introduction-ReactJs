c1e8b0e7c04d068beac229ddcd66fb0d8a52eae1
Vinicius <vinisilva45@live.com>
2020-10-22 16:43:32 -0300

    Exemplo de soma.

--- configuration/src/index.js
+++ configuration/src/index.js
@@ -1,13 +1,27 @@
+import { func } from "prop-types"
 import React from "react"
 import ReactDOM from "react-dom"
 import "./styles.css"
 
+function sum(a,b){
+    return a + b;
+}
+
+
+function primeiroJSX(){
+    return(
+        <div>
+            Vinicius Rosa - Introdução ao ReactJS
+            <h1>Soma: {sum(10, 10)}</h1>
+        </div>
+    )
+}
 
 const App = () => {
 
   return (
     <div className="App">
-      Hello World
+      {primeiroJSX()}
     </div>
   )
 }
